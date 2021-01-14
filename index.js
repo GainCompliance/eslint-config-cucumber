cd05638fad2cbcff1647dc6c78044ffbacc8ce97
Travis Peterson <contact@travispeterson.us>
2021-01-14 11:02:05 -0600

    fix: removes form8ion dependency

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-module.exports = {extends: '@form8ion/cucumber'};
+module.exports = {
+  plugins: ['cucumber'],
+
+  overrides: [{
+    files: 'test/integration/**/features/step_definitions/**/*-steps.js',
+
+    rules: {
+      'new-cap': [
+        'error',
+        {
+          capIsNewExceptions: [
+            'Before',
+            'BeforeAll',
+            'After',
+            'AfterAll',
+            'Given',
+            'When',
+            'Then'
+          ]
+        }
+      ],
+      'prefer-arrow-callback': 'off',
+      'cucumber/no-arrow-functions': 'error',
+      'func-names': 'off'
+    }
+  }]
+};
